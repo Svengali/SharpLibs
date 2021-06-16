3c8af2c469dcc1dfef8cb6aeef650d4ab754bc18
Marc Hernandez <marc.hernandez@disruptivegames.com>
2021-06-15 21:42:16 -0700

    Log changes

--- NetCore/net/Conn.cs
+++ NetCore/net/Conn.cs
@@ -44,9 +44,9 @@ namespace net
 		{
 			var curThread = Thread.CurrentThread;
 
-			lib.Log.logProps( curThread, $"Starting thread { curThread.Name} ({ curThread.ManagedThreadId})", lib.LogType.Info );
+			log.logProps( curThread, $"Starting thread { curThread.Name} ({ curThread.ManagedThreadId})", log.LogType.Info );
 
-			lib.Log.logProps( Sock, $"Socket {Sock.RemoteEndPoint}", lib.LogType.Info );
+			log.logProps( Sock, $"Socket {Sock.RemoteEndPoint}", log.LogType.Info );
 
 
 
@@ -58,7 +58,7 @@ namespace net
 					uint len = BitConverter.ToUInt32( mm_recvBuf, 0 );
 					uint totalAmountRead = 0;
 
-					//lib.Log.info( "Len {0} needed to read", len );
+					//log.info( "Len {0} needed to read", len );
 
 					var memStream = new MemoryStream( 1024 );
 
@@ -90,7 +90,7 @@ namespace net
 						memStream.Position = 0;
 
 						//var str = System.Text.Encoding.Default.GetString( memStream.GetBuffer() );
-						//lib.Log.info( "Received {0} in obj {1}.", totalAmountRead, str );
+						//log.info( "Received {0} in obj {1}.", totalAmountRead, str );
 
 						var obj = recieveObject( memStream );
 						if( obj != null )
@@ -103,28 +103,28 @@ namespace net
 					}
 					catch( Exception e )
 					{
-						lib.Log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
+						log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
 					}
 				}
 				catch( SocketException e )
 				{
-					lib.Log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
+					log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
 					m_loop = false;
 				}
 				catch( IOException e )
 				{
-					lib.Log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
+					log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
 					m_loop = false;
 				}
 
 				if( !Sock.Connected )
 				{
-					lib.Log.info( $"Socket is no longer connected, leaving" );
+					log.info( $"Socket is no longer connected, leaving" );
 					m_loop = false;
 				}
 			}
 
-			lib.Log.info( $"Ending thread { curThread.Name} ({ curThread.ManagedThreadId})" );
+			log.info( $"Ending thread { curThread.Name} ({ curThread.ManagedThreadId})" );
 		}
 
 
@@ -142,12 +142,12 @@ namespace net
 		void asyncRecv( IAsyncResult ar )
 		{
 			int packetSize = Stream.EndRead( ar );
-			lib.Log.info( $"Recieved {packetSize}" );
+			log.info( $"Recieved {packetSize}" );
 
 			if( !Sock.Connected || packetSize == 0 )
 			{
 				var stuff = Sock.Connected?"Connected":"Disconnected";
-				lib.Log.warn( $"Socket {Sock.RemoteEndPoint} had a problem. {stuff} recv {packetSize} bytes." );
+				log.warn( $"Socket {Sock.RemoteEndPoint} had a problem. {stuff} recv {packetSize} bytes." );
 				m_loop = false;
 				return;
 			}
@@ -155,7 +155,7 @@ namespace net
 			/*
 			for( int i = 0; i < packetSize; ++i )
 			{
-				lib.Log.info( "{0}:{1}:{2}", i, mm_recvBuf[ i ], (char)mm_recvBuf[ i ] );
+				log.info( "{0}:{1}:{2}", i, mm_recvBuf[ i ], (char)mm_recvBuf[ i ] );
 			}
 			*/
 
@@ -169,12 +169,12 @@ namespace net
 				recv.Position = 0;
 
 				var str = System.Text.Encoding.Default.GetString( mm_recvBuf );
-				lib.Log.info( $"Received {packetSize} in obj {str}." );
+				log.info( $"Received {packetSize} in obj {str}." );
 
 				/*
 				for( int i = 0; i < packetSize; ++i )
 				{
-					lib.Log.info( "{0,3}:{1,3}:{2}:{3,3}", i, mm_recvBuf[ i ], (char)mm_recvBuf[ i ], mm_memRecvStream.ReadByte() );
+					log.info( "{0,3}:{1,3}:{2}:{3,3}", i, mm_recvBuf[ i ], (char)mm_recvBuf[ i ], mm_memRecvStream.ReadByte() );
 				}
 				*/
 
@@ -189,17 +189,17 @@ namespace net
 			}
 			catch( SocketException e )
 			{
-				lib.Log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
+				log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
 				m_loop = false;
 			}
 			catch( IOException e )
 			{
-				lib.Log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
+				log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
 				m_loop = false;
 			}
 			catch( Exception e )
 			{
-				lib.Log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
+				log.error( $"Socket {Sock.RemoteEndPoint} had a problem.  Ex {e}." );
 			}
 
 			mm_recvWait.Set();
